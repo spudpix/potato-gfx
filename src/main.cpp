bf7a31d9d6447cf2be85a8f5a956074698255fd2
PixelPotato <s.leung@outlook.fr>
2021-06-25 16:25:06 +0100

    Added EBO

--- src/main.cpp
+++ src/main.cpp
@@ -101,17 +101,25 @@ int main()
 	// Vertex data
 	//-------------------------------------------------
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
 	};
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
 
 	//-------------------------------------------------
 	// OpenGL buffers
 	//-------------------------------------------------
 
-	unsigned int VBO, VAO;
+    unsigned int VBO;
+    unsigned int VAO;
+    unsigned int IBO;
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO); 	// OpenGL core profile requires a VAO to be bound
@@ -119,6 +127,10 @@ int main()
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);		// Copies the data from system RAM to GPU RAM
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	// Defines the data layout of the VBO and stores it in the VAO
 	glEnableVertexAttribArray(0);
@@ -136,7 +148,7 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
